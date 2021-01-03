a3c74e29fcb5141dab96e6c2669eddb70abc068f
Yeseul O <yeseulo@gmail.com>
2021-01-03 23:05:36 +0900

    Add sign in/sign up process

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Router from 'components/Router';
 import { authService } from 'firebaseSetting';
 
 function App() {
-  const [isLoggedIn, setisLoggedIn] = useState(authService.currentUser);
-  return <Router isLoggedIn={isLoggedIn} />;
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  return <>{init ? <Router isLoggedIn={isLoggedIn} /> : 'Initailizing...'}</>;
 }
 
 export default App;
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import { authService } from 'firebaseSetting';
+import React, { useState } from 'react';
 
 const Auth = () => {
-  return <div>auth</div>;
+  const [isNewUser, setNewUser] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    if (name === 'email') setEmail(value);
+    else if (name === 'password') setPassword(value);
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (isNewUser) {
+        await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const onToggle = () => setNewUser((prevState) => !prevState);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name='email'
+          type='email'
+          placeholder='Email'
+          value={email}
+          required
+          onChange={onChange}
+        />
+        <input
+          name='password'
+          type='password'
+          placeholder='Password'
+          value={password}
+          required
+          onChange={onChange}
+        />
+        <button type='submit'>{isNewUser ? `Sign up` : `Sign in`}</button>
+        {error && <p>{error}</p>}
+      </form>
+      <span onClick={onToggle}>{isNewUser ? `Sign in` : `Sign up`}</span>
+      <div>
+        <button type='button'>Continue with Google</button>
+        <button type='button'>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
