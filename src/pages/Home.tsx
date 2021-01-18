b2b3ef3014cb10c545222c137b8262225152c05f
Yeseul O <yeseulo@gmail.com>
2021-01-18 23:11:11 +0900

    Add form for saving sentence to firestore

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from 'firebaseSetting';
 
 const Home = () => {
+  const [sentence, setSentence] = useState('');
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await dbService.collection('sentences').add({
+      sentence,
+      createdAt: Date.now(),
+    });
+    setSentence('');
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSentence(e.target.value);
+  };
+
   return (
     <div>
-      home
+      <form onSubmit={onSubmit}>
+        <input
+          type='text'
+          placeholder="What's your sentence?"
+          maxLength={250}
+          value={sentence}
+          onChange={onChange}
+        />
+        <button type='submit'>Save</button>
+      </form>
     </div>
   );
 };
